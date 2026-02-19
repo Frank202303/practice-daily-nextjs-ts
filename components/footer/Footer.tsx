922bb1331e8e457aaedfb589a75afa8272a313cf
frankzhang <frank.zhang.sa.au@gmail.com>
2026-02-19 16:55:07 +1000

    Finish home page: todo -- responsive

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <p className={styles.logo}>frankdev</p>
+      <p className={styles.text}>
+        Frank creative thoughts agency © All rights reserved.
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
