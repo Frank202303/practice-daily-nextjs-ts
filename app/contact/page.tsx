cbaa3d31eeeb9a87b74972e0f3fece20f31fe652
frankzhang <frank.zhang.sa.au@gmail.com>
2026-02-24 16:43:13 +1000

    Finish desktop UI

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./contact.module.css";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src={"/contact.png"} alt="contact" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" name="name" placeholder="Full Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <input
+            type="text"
+            name="phone"
+            placeholder="Phone Number (optional)"
+          />
+          <textarea rows={6} placeholder="Message" />
+          <button>Send</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default ContactPage;
